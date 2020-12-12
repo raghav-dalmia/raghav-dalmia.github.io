92e0410fc0894d873a9cd7711768b9441791dd96
raghav <raghav.ec.17@nsit.net.in>
2020-12-12 23:33:19 +0530

    init commit for responsive site

--- js/js_script.js
+++ js/js_script.js
@@ -1,53 +1,13 @@
-$(".jumbotron").css({ height: $(window).height() + "px" });
-$("#aboutMe-card").css({ minHeight: ($(window).height()*0.75) + "px"});
- 
+// typing text effect
+var content = 'A software developer & competitive coder. ';
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
-$(window).on("resize", function() {
-  $(".jumbotron").css({ height: $(window).height() + "px" });
-  $("#aboutMe-card").css({ minHeight: ($(window).height()*0.75) + "px"});
+$(ele).hide().appendTo('#hero').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
 });
 
-const header = document.querySelector("header");
-const sectionSiteHero = document.querySelector(".jumbotron");
-const sectionEducation = document.querySelector("#myWebsite");
-const whatsappLogo = document.getElementById("whatsappLogo");
-
-const sectionNavOptions = {
-    rootMargin: "-200px 0px 0px 0px"
-};
-
-const scrollbarObserver = new IntersectionObserver(function(
-    entries,
-    scrollbarObserver
-){
-    entries.forEach(entry => {
-        console.log(entry.intersectionRatio);
-        if(!entry.isIntersecting){
-            header.classList.add("nav-scrolled");
-        }
-        else{
-            header.classList.remove("nav-scrolled");
-        }
-    });
-},
-sectionNavOptions);
-scrollbarObserver.observe(sectionSiteHero);
-
-const scrollbarObserver2 = new IntersectionObserver(function(
-    entries,
-    scrollbarObserver2
-){
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            whatsappLogo.style.visibility = "hidden";
-            whatsappLogo.style.opacity = 0;
-            whatsappLogo.style.transition = "visibility 0.5s, opacity 0.5s linear";
-        }
-        else{
-            whatsappLogo.style.visibility = "initial";
-            whatsappLogo.style.opacity = 1;
-            whatsappLogo.style.transition = "visibility 0.5s, opacity 0.5s linear";
-        }
-    });
-}, sectionNavOptions);
-scrollbarObserver2.observe(sectionEducation);
